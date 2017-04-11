6d86935047c17b5d85d62465c317d76ca344a700
Devin Nathan-Turner <devin.nathan-turner@turnerd-mbp.local>
2017-04-10 22:54:23 -0400

    Limit to 100th prime at 541, but get all tests to pass

--- nth_prime.exs
+++ nth_prime.exs
@@ -5,6 +5,18 @@ defmodule Prime do
   """
   @spec nth(non_neg_integer) :: non_neg_integer
   def nth(count) do
+    _nth(count)
   end
 
+  defp _nth(num) when num < 1, do: raise "Require non-negative integer larger than 0"
+  defp _nth(num) do
+    Enum.at(2..542 |> Enum.filter(fn i -> is_prime?(i) end), num-1)
+  end
+
+  defp is_prime?(2), do: true
+  defp is_prime?(number) do
+    2..number
+    |> Enum.filter(fn i -> rem(number, i) == 0 end)
+    |> length() == 1
+  end
 end
